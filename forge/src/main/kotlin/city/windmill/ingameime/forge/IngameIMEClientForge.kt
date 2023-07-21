397a233c3a0d4c616ae2e2513b6ead611512c579
TexTrue <3140846162@qq.com>
2023-07-21 14:58:16 +0800

    clean

--- forge/src/main/kotlin/city/windmill/ingameime/forge/IngameIMEClientForge.kt
+++ forge/src/main/kotlin/city/windmill/ingameime/forge/IngameIMEClientForge.kt
@@ -20,16 +20,11 @@ import dev.architectury.platform.forge.EventBuses
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry
 import net.minecraft.Util
 import net.minecraft.client.Minecraft
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent
 import net.minecraftforge.fml.IExtensionPoint
-import net.minecraftforge.fml.ModList
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import net.minecraftforge.network.NetworkConstants
 import thedarkcolour.kotlinforforge.forge.LOADING_CONTEXT
-import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.runForDist
 
 @Mod(IngameIME.MODID)
@@ -46,24 +41,21 @@ object IngameIMEClientForge {
     init {
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
         EventBuses.registerModEventBus(IngameIME.MODID, LOADING_CONTEXT.extension())
-        //LOADING_CONTEXT.registerExtensionPoint(IExtensionPoint.DisplayTest::class.java) {
-        //    IExtensionPoint.DisplayTest(NetworkConstants::IGNORESERVERONLY) { _, _ -> true }
-        //}
+        LOADING_CONTEXT.registerExtensionPoint(IExtensionPoint.DisplayTest::class.java) {
+            return@registerExtensionPoint IExtensionPoint.DisplayTest(NetworkConstants::IGNORESERVERONLY) { _, _ -> true }
+        }
 
         runForDist({
             if (Util.getPlatform() == Util.OS.WINDOWS) {
                 IngameIME.LOGGER.info("it is Windows OS! Loading mod...")
 
                 modEventBus.addListener(::onInitializeClient)
-                /*
-                with(INGAMEIME_BUS) {
-                    addListener(::registerKeys)
-                    addListener(::enqueueIMC)
-                }
-                 */
-            } else
+            } else {
                 IngameIME.LOGGER.warn("This mod cant work in ${Util.getPlatform()} !")
-        }) { IngameIME.LOGGER.warn("This mod cant work in a DelicateServer!") }
+            }
+        }) {
+            IngameIME.LOGGER.warn("This mod cant work in a DelicateServer!")
+        }
     }
 
     private fun onInitializeClient(event: FMLClientSetupEvent) {
@@ -72,7 +64,7 @@ object IngameIMEClientForge {
         ClientLifecycleEvent.CLIENT_STARTED.register(ClientLifecycleEvent.ClientState {
             ConfigHandler.initialConfig()
 
-            ClientGuiEvent.RENDER_POST.register(ClientGuiEvent.ScreenRenderPost { screen, graphics, mouseX, mouseY, delta ->
+            ClientGuiEvent.RENDER_POST.register(ClientGuiEvent.ScreenRenderPost { _, graphics, mouseX, mouseY, delta ->
                 //Track mouse move here
                 if (mouseX != prevX || mouseY != prevY) {
                     ScreenEvents.SCREEN_MOUSE_MOVE.invoker().onMouseMove(prevX, prevY, mouseX, mouseY)
@@ -86,13 +78,13 @@ object IngameIMEClientForge {
             ScreenEvents.SCREEN_MOUSE_MOVE.register(ScreenEvents.MouseMove { _, _, _, _ ->
                 IMEHandler.IMEState.onMouseMove()
             })
-            ClientScreenInputEvent.KEY_PRESSED_PRE.register(ClientScreenInputEvent.KeyPressed { client, screen, keyCode, scanCode, modifiers ->
+            ClientScreenInputEvent.KEY_PRESSED_PRE.register(ClientScreenInputEvent.KeyPressed { _, _, keyCode, scanCode, modifiers ->
                 if (KeyHandler.KeyState.onKeyDown(keyCode, scanCode, modifiers))
                     EventResult.interruptDefault()
                 else
                     EventResult.pass()
             })
-            ClientScreenInputEvent.KEY_RELEASED_PRE.register(ClientScreenInputEvent.KeyReleased { client, screen, keyCode, scanCode, modifiers ->
+            ClientScreenInputEvent.KEY_RELEASED_PRE.register(ClientScreenInputEvent.KeyReleased { _, _, keyCode, scanCode, modifiers ->
                 if (KeyHandler.KeyState.onKeyUp(keyCode, scanCode, modifiers))
                     EventResult.interruptDefault()
                 else
@@ -126,46 +118,4 @@ object IngameIMEClientForge {
         //Ensure native dll are loaded, or crash the game
         IngameIME.LOGGER.info("Current IME State:${ExternalBaseIME.State}")
     }
-
-    /*
-    private fun registerKeys(event: RegisterKeyMappingsEvent) {
-        event.register(KeyHandler.toggleKey)
-    }
-
-    @Suppress("UNUSED_PARAMETER")
-    private fun enqueueIMC(event: InterModEnqueueEvent) {
-        with(FORGE_BUS) {
-            addListener<ScreenEvent.Render.Post> {
-                OverlayScreen.render(it.guiGraphics, it.mouseX, it.mouseY, it.partialTick)
-            }
-            addListener<ScreenEvent.KeyPressed.Pre> {
-                it.isCanceled = KeyHandler.KeyState.onKeyDown(it.keyCode, it.scanCode, it.modifiers)
-            }
-            addListener<ScreenEvent.KeyReleased.Pre> {
-                it.isCanceled = KeyHandler.KeyState.onKeyUp(it.keyCode, it.scanCode, it.modifiers)
-            }
-        }
-        with(INGAMEIME_BUS) {
-            addListener<LegacyScreenEvents.WindowSizeChanged> {
-                ExternalBaseIME.FullScreen = Minecraft.getInstance().window.isFullscreen
-            }
-            addListener<LegacyScreenEvents.ScreenChanged> {
-                ScreenHandler.ScreenState.onScreenChange(it.oldScreen, it.newScreen)
-            }
-            addListener<LegacyScreenEvents.EditOpen> {
-                ScreenHandler.ScreenState.EditState.onEditOpen(it.edit, it.caretPos)
-            }
-            addListener<LegacyScreenEvents.EditCaret> {
-                ScreenHandler.ScreenState.EditState.onEditCaret(it.edit, it.caretPos)
-            }
-            addListener<LegacyScreenEvents.EditClose> {
-                ScreenHandler.ScreenState.EditState.onEditClose(it.edit)
-            }
-        }
-        ConfigHandler.initialConfig()
-
-        //Ensure native dll are loaded, or crash the game
-        IngameIME.LOGGER.info("Current IME State:${ExternalBaseIME.State}")
-    }
-     */
 }
